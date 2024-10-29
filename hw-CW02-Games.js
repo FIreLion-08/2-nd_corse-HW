157c270b45cc7811de37c91f0aa9c2573f9b8279
Dmitry Avdoshkin <works-dim@yandex.ru>
2024-10-29 10:26:13 +0800

    HW-CW02_v1

--- hw-CW02-Games.js
+++ hw-CW02-Games.js
@@ -156,3 +156,29 @@ function gameRockPaperScissors() {
 
 
 //Описание игры для сайта («Генератор случайных цветов»)
+// Функция для изменения цвета фона у элементов с классами game-about и games
+function gameBackgroundColor() {
+  const gameaboutElement = document.querySelectorAll('.game-about');
+  const gamesElement = document.querySelectorAll('.games');
+
+// Генерируем случайный цвет
+  const randomColor = getRandomColor();
+
+  // Применяем один и тот же цвет ко всем элементам
+  gameaboutElement.forEach(element => {
+      element.style.backgroundColor = randomColor;
+  });
+  gamesElement.forEach(element => {
+      element.style.backgroundColor = randomColor;
+  });
+}
+
+// Функция для генерации случайного цвета
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
